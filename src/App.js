29e4a8e8604ee5b55607aee3897fe87babfcf1b9
devwichrowski <damian1492>
2020-02-09 19:48:43 +0100

    Add layout to main app component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import "./App.scss"
-import {Route, Router} from "react-router-dom";
-import history from "./core/history";
+import {BrowserRouter, Route, Router} from "react-router-dom";
 import WelcomeView from "./views/WelcomeView/WelcomeView";
 import QuizView from "./views/QuizView/QuizView";
 import SummaryView from "./views/SummaryView/SummaryView";
+import Layout from "./components/shared/Layout/Layout";
 
 const App = () => {
     return (
         <div className="app">
-            <Router history={history}>
-                <Route exact path="/" component={WelcomeView}/>
-                <Route path="/quiz" component={QuizView}/>
-                <Route path="/summary" component={SummaryView}/>
-            </Router>
+            <BrowserRouter>
+                <Layout>
+                    <Route exact path="/" component={WelcomeView}/>
+                    <Route path="/quiz" component={QuizView}/>
+                    <Route path="/summary" component={SummaryView}/>
+                </Layout>
+            </BrowserRouter>
         </div>
     );
 };
